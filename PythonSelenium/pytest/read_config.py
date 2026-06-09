76cc1efbf84cf2c07d064bbf640c6222c5e1a071
samihamuhabathulla-jpg <samihamuhabathulla@gmail.com>
2026-06-09 21:09:51 +0530

    Update read_config.py

--- PythonSelenium/pytest/read_config.py
+++ PythonSelenium/pytest/read_config.py
@@ -1,7 +1,14 @@
 from configparser import ConfigParser
-import configparser
+import os
 
-def get_config(category,key):
+def get_config(category, key):
     config = ConfigParser()
-    config.read("./config.ini")
-    return config.get(category,key)
+
+    config_path = os.path.join(
+        os.path.dirname(__file__),
+        "config.ini"
+    )
+
+    config.read(config_path)
+
+    return config.get(category, key)
